5f0eb4f97c5d7bd6205106b699045e04978ed8d6
Stefan Patelski <stefanpatelski@gmail.com>
2020-05-16 22:58:45 +0200

    additional fixes for heroku

--- 0_your_first_server/first_server.py
+++ 0_your_first_server/first_server.py
@@ -7,7 +7,4 @@ def index():
     return 'Hello world!'
 
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
