8bcd3b94da4934b1ab8a3bd0cc7624ff200e9938
Stefan Patelski <stefanpatelski@gmail.com>
2020-05-16 22:55:18 +0200

    alter some settings to make it work on heroku

--- 0_your_first_server/first_server.py
+++ 0_your_first_server/first_server.py
@@ -1,3 +1,4 @@
+import os
 from bottle import route, run
 
 
@@ -6,4 +7,7 @@ def index():
     return 'Hello world!'
 
 
-run(host='localhost', port=8080)
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080)
